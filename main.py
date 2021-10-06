5929a1409f06cece10432c4366dd4df8253906fe
Guadalupe Reyes <reyesg29@uabc.edu.mx>
2021-10-05 17:37:50 -0700

    Modulos de python

---
+++ main.py
@@ -0,0 +1,14 @@
+#import operations
+#print(operations.rest(10,5))
+
+from operations import sum,rest,mensaje
+print(sum(10,5))
+print(rest(10,5))
+print(mensaje)
+
+from operations import *
+print(sum(10,5))
+print(rest(10,5))
+print(mensaje)
+
+
---
+++ operations.py
@@ -0,0 +1,14 @@
+def sum(num1,num2):
+    return num1+num2
+
+def rest(num1,num2):
+    return num1-num2
+
+def mult(num1,num2):
+    return num1*num2
+
+def div(num1,num2):
+    return num1//num2
+
+mensaje="hola mundo"
+
