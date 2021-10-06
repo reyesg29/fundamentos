52c218d42f90057d68c67719cd695a141b16248f
Guadalupe Reyes <reyesg29@uabc.edu.mx>
2021-10-05 17:08:59 -0700

    Cadenas de python

---
+++ origin.py
@@ -0,0 +1,17 @@
+name='guadalupe reyes martinez'
+age=32
+istall=False
+height=1.52
+
+print(name + " tiene "+str(age)+" años")
+print("{} tiene {} años".format(name,age))
+
+
+print(f"{name} tiene {age+10} años y es \
+alto {istall} y mide {height}")
+
+print(name.upper())
+print(name.lower())
+print(name.title())
+
+print(name*5)
